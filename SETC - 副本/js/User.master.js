2f7978c5452400f83c465c2dfc7dd116e861f002
ZRUIK <1799726492@qq.com>
2016-07-28 16:58:48 +0800

    删除副本

--- SETC - 副本/js/User.master.js
+++
@@ -1,3 +0,0 @@
-﻿$(document).ready(function() {
-    $("#SideBar li").hover(function() { $(this).css({ "background-color": "#ccc" }); }, function() { $(this).css({ "background-color": "#fff" }); });
-});
